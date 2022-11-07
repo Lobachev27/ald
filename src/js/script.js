dec25c06c4f1ccd0c4bba56a2b9256b21024fdd8
Dmitriy Lobachev <lobachevd27@gmail.com>
2022-11-07 17:49:26 +0300

    Added personal area for driver

--- src/js/script.js
+++ src/js/script.js
@@ -46,3 +46,65 @@ $(document).ready(function() {
     $(this).closest(".login-tabs__container").find(".login-form-contr").removeClass('login-form-hidden');
   });
 });
+
+/*Регистрация / авторизация в модалке водителя*/
+
+$(document).ready(function() {
+  $(function () {
+    var tabContainers = $('.login-tabs__container');
+    $('.js-md-tab').click(function () {
+      tabContainers.hide();
+      tabContainers.filter(this.hash).show();
+      $('.js-md-tab').removeClass('active');
+      $(this).addClass('active');
+      return false;
+    });
+
+    var url_string = window.location.href;
+    var id = url_string.split('id=')[1];
+    if (id === "events") {
+      $('.js-md-tab').filter(':last').click();
+    }
+  });
+
+  $(".js-md-tab").click(function () {
+    $(this).closest(".login-tabs").find(".modal-driver__login").removeClass('modal-driver__login-hidden');
+    $(this).closest(".login-tabs__buttons").addClass('login-tabs__buttons-hidden');
+  });
+});
+
+/*Табы в модалке для водителей*/
+
+$(document).ready(function() {
+  $(function () {
+    var tabContainers = $('.modal-driver__container');
+    tabContainers.hide().filter(':first').show();
+    $('.modal-driver__tab').click(function () {
+      tabContainers.hide();
+      tabContainers.filter(this.hash).show();
+      $('.modal-driver__tab').removeClass('active');
+      $(this).addClass('active');
+      return false;
+    }).filter(':first').click();
+
+    var url_string = window.location.href;
+    var id = url_string.split('id=')[1];
+    if (id === "events") {
+      $('.modal-driver__tab').filter(':last').click();
+    }
+  });
+});
+
+/*Модалка водителя*/
+
+$(document).ready(function () {
+  $(".user").click(function () {
+    $(".modal-driver").addClass("active");
+    $("body").addClass("ov-hid");
+  });
+
+  $(".modal-driver__close").click(function () {
+    $(".modal-driver").removeClass("active");
+    $("body").removeClass("ov-hid");
+  });
+});
